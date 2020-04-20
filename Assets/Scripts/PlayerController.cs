c6e826bed58f9b4c228b7f07f5c764c2aec3be43
Ben <benmorris930@gmail.com>
2020-04-20 11:02:20 -0700

    1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    public GameObject focalPoint;
+    public float speed = 10;
+    public float maxSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //if (rbPlayer.velocity.magnitude > maxSpeed) rbPlayer.velocity.magnitude = 5;
+        rbPlayer.AddForce(focalPoint.transform.forward * speed * verticalInput, ForceMode.Force);
+        rbPlayer.AddForce(focalPoint.transform.right * speed * horizontalInput, ForceMode.Force);
+
+        
+    }
+}
