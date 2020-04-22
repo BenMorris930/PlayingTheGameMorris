a49d9d206cbacba420daa10aa32b1aa667d48212
Ben <benmorris930@gmail.com>
2020-04-22 10:40:18 -0700

    4B

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private Rigidbody enemyRB;
+    public GameObject playerObject;
+    public float speed = 7;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyRB = GetComponent<Rigidbody>();
+        playerObject = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveDir = (playerObject.transform.position - transform.position).normalized;
+        Vector3 noY = new Vector3(moveDir.x, 0, moveDir.z);
+        enemyRB.AddForce(noY * speed);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject playerObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 3, 3);
+;    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemyPrefab, -playerObject.transform.position, enemyPrefab.transform.rotation);
+    }
+}
